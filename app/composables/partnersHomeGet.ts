c8d5f13dd7ae53d9b0f1ed118621afb70786e55c
jfsenechal <jfsenechal@gmail.com>
2025-07-28 11:14:11 +0200

    up

--- app/composables/partnersHomeGet.ts
+++ app/composables/partnersHomeGet.ts
@@ -1,43 +1,43 @@
 export const partnersHomeGet = [
     {
         url: "https://cap.marche.be",
-        img: "images/img/img_sponsor02.png",
+        img: "/images/img/img_sponsor02.png",
         alt: "cap commerçant",
         id: 1,
     },
     {
         url: "https://www.visitmarche.be",
-        img: "images/img/visit_logo_b.png",
+        img: "/images/img/visit_logo_b.png",
         alt: "visit marche",
         id: 2,
     },
     {
         url: "/economie",
-        img: "images/img/img_sponsor04.png",
+        img: "/images/img/img_sponsor04.png",
         alt: "economie",
         id: 3,
     },
     {
         url: "http://www.paysdefamenne.be",
-        img: "images/img/img_sponsor03.png",
+        img: "/images/img/img_sponsor03.png",
         alt: "pays de famenne",
         id: 4,
     },
     {
         url: "https://www.famenneardenne.be",
-        img: "images/img/img_sponsor01.png",
+        img: "/images/img/img_sponsor01.png",
         alt: "famenne ardenne",
         id: 5,
     },
     {
         url: "https://mcfa.marche.be",
-        img: "images/img/img_sponsor05.png",
+        img: "/images/img/img_sponsor05.png",
         alt: "mcfa",
         id: 6,
     },
     {
         url: "https://artistes.marche.be",
-        img: "images/img/artistes.png",
+        img: "/images/img/artistes.png",
         alt: "artistes",
         id: 7,
     }
