946bb367a823069a32cb6e0ed042648190143e44
jfsenechal <jfsenechal@gmail.com>
2025-07-29 13:20:37 +0200

    add base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
                 rel: 'stylesheet',
                 href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
             }]
-        }
+        },
+        baseURL: process.env.NUXT_APP_BASE_URL || '/',
     }
 })
