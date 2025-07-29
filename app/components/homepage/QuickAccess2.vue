02b639e89ded3aa37e35c4ae5e347e2dff8703f0
jfsenechal <jfsenechal@gmail.com>
2025-07-29 14:40:05 +0200

    up

--- app/components/homepage/QuickAccess2.vue
+++ app/components/homepage/QuickAccess2.vue
@@ -4,37 +4,7 @@ import QuickAccessItem from "~/components/homepage/QuickAccessItem.vue";
 
 const shortcuts = [
   {
-    name: 'E-guichet',
-    description: 'Géolocalisation des parkings, infrastructures, environnement...',
-    // NOTE: 'fa-2x' has been removed. We'll control size with Tailwind.
-    icon: 'fas fa-id-card',
-    url: '/tourisme/article/20644',
-    id: 1,
-    color: 'text-blue-600',
-    bgcolor: 'bg-blue-100',
-    bgcolorhover: 'group-hover:bg-blue-600'
-  },
-  {
-    name: 'Parkings',
-    description: 'Chantiers en cours, les restrictions de circulation et les arrêtés impactant la voirie.',
-    icon: 'fas fa-square-parking',
-    url: '/citoyen/category/119',
-    id: 2,
-    color: 'text-yellow-600',
-    bgcolor: 'bg-yellow-100',
-    bgcolorhover: 'group-hover:bg-yellow-600'
-  },
-  {
-    name: 'Piscine', description: 'Horaires et tarifs.',
-    icon: 'fas fa-person-swimming',
-    url: '/sport/article/71/5153',
-    id: 4,
-    color: 'text-red-600',
-    bgcolor: 'bg-red-100',
-    bgcolorhover: 'group-hover:bg-red-600'
-  },
-  {
-    name: 'Horaires-Contacts',
+    name: 'Horaire-Contact',
     description: 'Prenez rendez-vous...',
     icon: 'fas fa-clock',
     url: '/citoyen/category/38',
@@ -44,7 +14,37 @@ const shortcuts = [
     bgcolorhover: 'group-hover:bg-green-600'
   },
   {
-    name: 'Enfance - Jeunesse', description: 'Activités, écoles et aides',
+    name: 'E-guichet',
+    description: 'Géolocalisation des parkings, infrastructures, environnement...',
+    // NOTE: 'fa-2x' has been removed. We'll control size with Tailwind.
+    icon: 'fas fa-id-card',
+    url: '/tourisme/article/20644',
+    id: 1,
+    color: 'text-orange-600',
+    bgcolor: 'bg-orange-100',
+    bgcolorhover: 'group-hover:bg-orange-600'
+  },
+  {
+    name: 'Piscine', description: 'Horaires et tarifs.',
+    icon: 'fas fa-person-swimming',
+    url: '/sport/article/71/5153',
+    id: 4,
+    color: 'text-blue-600',
+    bgcolor: 'bg-blue-100',
+    bgcolorhover: 'group-hover:bg-blue-600'
+  },
+  {
+    name: 'Parking',
+    description: 'Chantiers en cours, les restrictions de circulation et les arrêtés impactant la voirie.',
+    icon: 'fas fa-square-parking',
+    url: '/citoyen/category/119',
+    id: 2,
+    color: 'text-yellow-600',
+    bgcolor: 'bg-yellow-100',
+    bgcolorhover: 'group-hover:bg-yellow-600'
+  },
+  {
+    name: 'Enfance Jeunesse', description: 'Activités, écoles et aides',
     icon: 'fas fa-child',
     color: 'text-indigo-600',
     bgcolor: 'bg-indigo-100',
